09f04e0ac2a20dd7a39f82d1516db9f8bf01f699
Patrick Brennan <padist3r@gmail.com>
2020-11-27 11:53:43 +0000

    added snake class

--- main.py
+++ main.py
@@ -1,22 +1,60 @@
 import pygame
 
-pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Snake.. Snake!? SNAAAKKKEEE!!!!")
+class Snake:
 
-# colors
-white = (255, 255, 255)
+    def __init__(self, display, colour, sw, sh):
+        self.x = 400
+        self.y = 300
+        self.dx = 0
+        self.dy = 10
+        self.dir = ""
+        self.size = 10
+        self.sw = sw
+        self.sh = sh
+        self.screen = display
+        self.colour = colour
+        self.body = pygame.image.load("Images/snake.png")
 
-game_over = False
-while not game_over:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_over = True
-        print(event)
+    def draw_snake(self):
+        self.screen.fill((0, 0, 0))
+        self.screen.blit(self.body, (self.x, self.y))
+        self.movement()
 
-    pygame.draw.rect(screen, white, [400, 300, 10, 10])
-    pygame.display.update()
+    def movement(self):
+        pass
 
-pygame.quit()
-quit()
+
+if __name__ == '__main__':
+
+    pygame.init()
+
+    # display
+    width = 800
+    height = 600
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Snake.. Snake!? SNAAAKKKEEE!!!!")
+
+    # colors
+    white = (255, 255, 255)
+
+    # snake
+    snake = Snake(screen, white, width, height)
+
+    # clock
+    clock = pygame.time.Clock()
+
+    game_over = False
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            print(event)
+
+        snake.draw_snake()
+
+        pygame.display.update()
+        clock.tick(10)
+
+    pygame.quit()
+    quit()
