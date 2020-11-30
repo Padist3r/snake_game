b24aecb7ae8170e741c407794979243f5c3d2287
Patrick Brennan <padist3r@gmail.com>
2020-11-30 15:53:12 +0000

    snake is moving

--- main.py
+++ main.py
@@ -8,20 +8,36 @@ class Snake:
         self.y = 300
         self.dx = 0
         self.dy = 10
-        self.dir = ""
-        self.size = 10
+        self.direction = None
         self.sw = sw
         self.sh = sh
         self.screen = display
         self.colour = colour
         self.body = pygame.image.load("Images/snake.png")
 
-    def draw_snake(self):
+    def draw_snake(self, dxdy):
+        self.direction = dxdy
         self.screen.fill((0, 0, 0))
         self.screen.blit(self.body, (self.x, self.y))
         self.movement()
 
     def movement(self):
+        self.x += self.dx
+        self.y += self.dy
+        if self.direction == "w":
+            self.dx = 0
+            self.dy = -10
+        if self.direction == "a":
+            self.dx = -10
+            self.dy = 0
+        if self.direction == "s":
+            self.dx = 0
+            self.dy = 10
+        if self.direction == "d":
+            self.dx = 10
+            self.dy = 0
+
+    def tail(self):
         pass
 
 
@@ -40,6 +56,7 @@ if __name__ == '__main__':
 
     # snake
     snake = Snake(screen, white, width, height)
+    direction = None
 
     # clock
     clock = pygame.time.Clock()
@@ -49,12 +66,20 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
-            print(event)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == ord("w"):
+                    direction = "w"
+                elif event.key == ord("a"):
+                    direction = "a"
+                elif event.key == ord("s"):
+                    direction = "s"
+                elif event.key == ord("d"):
+                    direction = "d"
 
-        snake.draw_snake()
+        snake.draw_snake(direction)
 
         pygame.display.update()
-        clock.tick(10)
+        clock.tick(5)
 
     pygame.quit()
     quit()
