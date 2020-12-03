28a2ea6af409938f94f71963150b9091f34a1fd3
Patrick Brennan <padist3r@gmail.com>
2020-12-03 14:25:17 +0000

    added food and got snakes tail working

--- main.py
+++ main.py
@@ -1,46 +1,52 @@
 import pygame
+import random
 
 
 class Snake:
 
     def __init__(self, display, colour, sw, sh):
+        global direction
         self.x = 400
         self.y = 300
         self.dx = 0
-        self.dy = 10
-        self.direction = None
-        self.off_x = 0
-        self.off_y = -30
-        self.food_eaten = 4
+        self.dy = 20
+        self.jumps = 20
+        self.positions = [(self.x, self.y)]
+        self.food_eaten = 1
+        self.food_x = 200
+        self.food_y = 200
         self.sw = sw
         self.sh = sh
         self.screen = display
         self.colour = colour
         self.body = pygame.image.load("Images/snake.png")
 
-    def draw_snake(self, dxdy):
-        self.direction = dxdy
+    def draw_snake(self):
         self.screen.fill((0, 0, 0))
         self.screen.blit(self.body, (self.x, self.y))
+        self.food()
         self.movement()
         self.tail()
 
     def movement(self):
         self.x += self.dx
         self.y += self.dy
-        if self.direction == "w":
+        if direction == "w" and self.dy != self.jumps:
             self.dx = 0
-            self.dy = -10
+            self.dy = -self.jumps
 
-        if self.direction == "a":
-            self.dx = -10
+        if direction == "a" and self.dx != self.jumps:
+            self.dx = -self.jumps
             self.dy = 0
-        if self.direction == "s":
+
+        if direction == "s" and self.dy != -self.jumps:
             self.dx = 0
-            self.dy = 10
-        if self.direction == "d":
-            self.dx = 10
+            self.dy = self.jumps
+
+        if direction == "d" and self.dx != -self.jumps:
+            self.dx = self.jumps
             self.dy = 0
+
         # stops snake from disappearing off the screen
         if self.x == 0:
             self.x = self.sw
@@ -51,14 +57,23 @@ class Snake:
         elif self.y == self.sh:
             self.y = 0
 
+        self.positions.append((self.x, self.y))
+
     def tail(self):
-        if self.food_eaten == 0:
-            pass
-        else:
-            for i in range(1, self.food_eaten + 1):
-                self.screen.blit(self.body,
-                                 (self.x - (self.off_x * -i),
-                                  self.y - (self.off_y * -i)))
+        for i in range(1, self.food_eaten + 1):
+            self.screen.blit(self.body, self.positions[-i - 1])
+
+        # This is the maximum no. of squares pn the board. Start deleting
+        # positions to keep list to a minimum size
+        if len(self.positions) == 1200:
+            del self.positions[0]
+
+    def food(self):
+        self.screen.blit(self.body, (self.food_x, self.food_y))
+        if (self.x, self.y) == (self.food_x, self.food_y):
+            self.food_eaten += 1
+            self.food_x, self.food_y = 20 * random.randint(1, 39), \
+                                       20 * random.randint(1, 29)
 
 
 if __name__ == '__main__':
@@ -96,10 +111,10 @@ if __name__ == '__main__':
                 elif event.key == ord("d"):
                     direction = "d"
 
-        snake.draw_snake(direction)
+        snake.draw_snake()
 
         pygame.display.update()
-        clock.tick(5)
+        clock.tick(10)
 
     pygame.quit()
     quit()
