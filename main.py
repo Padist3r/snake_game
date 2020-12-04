f53c952bc19d4a26980ea8177378429aa1d8f286
Patrick Brennan <padist3r@gmail.com>
2020-12-04 16:43:05 +0000

    added border. changed snake from image to a pygame rect. snake stops on collision with tail

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ class Snake:
 
     def __init__(self, display, colour, sw, sh):
         global direction
+        self.height, self.width = 20, 20
         self.x = 400
         self.y = 300
         self.dx = 0
         self.dy = 20
         self.jumps = 20
         self.positions = [(self.x, self.y)]
+        self.snake_alive = True
         self.food_eaten = 1
         self.food_x = 200
         self.food_y = 200
@@ -19,11 +21,10 @@ class Snake:
         self.sh = sh
         self.screen = display
         self.colour = colour
-        self.body = pygame.image.load("Images/snake.png")
 
     def draw_snake(self):
-        self.screen.fill((0, 0, 0))
-        self.screen.blit(self.body, (self.x, self.y))
+        pygame.draw.rect(self.screen, self.colour,
+                         (self.x, self.y, self.height, self.width))
         self.food()
         self.movement()
         self.tail()
@@ -31,6 +32,9 @@ class Snake:
     def movement(self):
         self.x += self.dx
         self.y += self.dy
+
+        # this code lets you control snake and also stops him from turning
+        # back on himself
         if direction == "w" and self.dy != self.jumps:
             self.dx = 0
             self.dy = -self.jumps
@@ -48,32 +52,51 @@ class Snake:
             self.dy = 0
 
         # stops snake from disappearing off the screen
-        if self.x == 0:
-            self.x = self.sw
-        elif self.x == self.sw:
-            self.x = 0
-        elif self.y == 0:
-            self.y = self.sh
-        elif self.y == self.sh:
-            self.y = 0
+        if self.x == 20:
+            self.x = self.sw - 20
+        elif self.x == self.sw - 20:
+            self.x = 20
+        elif self.y == 20:
+            self.y = self.sh - 20
+        elif self.y == self.sh - 20:
+            self.y = 20
 
-        self.positions.append((self.x, self.y))
+        if self.snake_alive:
+            self.positions.append((self.x, self.y))
 
     def tail(self):
         for i in range(1, self.food_eaten + 1):
-            self.screen.blit(self.body, self.positions[-i - 1])
+            pygame.draw.rect(self.screen, self.colour,
+                             (self.positions[-i - 1][0],
+                              self.positions[-i - 1][1],
+                              self.height, self.width))
+            # if snake hits his tail, stop him moving as game is over
+            if self.positions[-i - 1] == (self.x, self.y):
+                self.snake_alive = False
+                self.dx = 0
+                self.dy = 0
 
         # This is the maximum no. of squares pn the board. Start deleting
-        # positions to keep list to a minimum size
+        # positions to keep list to a relatively low size.
+        # can probably make this more memory efficient but space is not an
+        # issue at the minute
         if len(self.positions) == 1200:
             del self.positions[0]
 
     def food(self):
-        self.screen.blit(self.body, (self.food_x, self.food_y))
+        pygame.draw.rect(self.screen, self.colour,
+                         (self.food_x, self.food_y, self.height, self.width))
         if (self.x, self.y) == (self.food_x, self.food_y):
             self.food_eaten += 1
-            self.food_x, self.food_y = 20 * random.randint(1, 39), \
-                                       20 * random.randint(1, 29)
+            self.food_x, self.food_y = 20 * random.randint(1, 38), \
+                                       20 * random.randint(1, 28)
+
+
+def main_screen(scr):
+    pygame.draw.line(scr, white, (20, 20), (780, 20))
+    pygame.draw.line(scr, white, (20, 580), (780, 580))
+    pygame.draw.line(scr, white, (20, 20), (20, 580))
+    pygame.draw.line(scr, white, (780, 20), (780, 580))
 
 
 if __name__ == '__main__':
@@ -111,10 +134,13 @@ if __name__ == '__main__':
                 elif event.key == ord("d"):
                     direction = "d"
 
+        screen.fill((0, 0, 0))
         snake.draw_snake()
+        print((snake.x,snake.y))
+        main_screen(screen)
 
         pygame.display.update()
-        clock.tick(10)
+        clock.tick(15)
 
     pygame.quit()
     quit()
